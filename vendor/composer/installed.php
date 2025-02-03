b95f9f7034bd8a8df37bda02a3579476c6504459
DmitriyChiroky <dwchirocky@gmail.com>
2025-02-03 10:40:13 +0200

    add

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dima/auth-system-wp',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '18bc26da764c22a01fb0da1f110b79bef5fdd4b2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dima/auth-system-wp' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '18bc26da764c22a01fb0da1f110b79bef5fdd4b2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
